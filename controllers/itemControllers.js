a25b398659c1b8a9238b88762c4a0195817e193e
Tomagochito <tbolanos@espol.edu.ec>
2024-07-15 14:11:18 -0500

    updateitem->updateitems

---
+++ controllers/itemControllers.js
@@ -0,0 +1,118 @@
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+exports.createItem = async (req, res) => {
+    try {
+        const data = req.body;
+        const itemRef = await db.collection('items').add(data);
+        res.status(201).send(`Created a new item: ${itemRef.id}`);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
+        /* 
+      #swagger.tags = ['Items']
+      #swagger.description = 'Create an item'
+      #swagger.summary = 'Create an item'
+      #swagger.parameters['data'] = {
+          in: 'body',
+          description: 'Data to create an item',
+          required: true,
+      }
+      #swagger.responses[201] = {
+          description: 'Item successfully created',
+      }
+      #swagger.responses[400] = {
+          description: 'Bad request',
+      }
+    */
+
+
+};
+
+exports.getAllItems = async (req, res) => {
+    try {
+        const itemsSnapshot = await db.collection('items').get();
+        const items = [];
+        itemsSnapshot.forEach((doc) => items.push({ id: doc.id, ...doc.data() }));
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
+       /* 
+     #swagger.tags = ['Items']
+     #swagger.description = 'Get all items entries'
+     #swagger.summary = 'Get all items entries'
+     #swagger.responses[200] = {
+         description: 'Items entries successfully obtained',
+     }
+     #swagger.responses[400] = {
+         description: 'Bad request',
+     }
+   */
+
+};
+
+
+
+
+exports.updateItem = async (req, res) => {
+
+   /* 
+     #swagger.tags = ['Items']
+     #swagger.description = 'Get all items entries'
+     #swagger.summary = 'Get all items entries'
+     #swagger.responses[200] = {
+         description: 'Items entries successfully obtained',
+     }
+     #swagger.responses[400] = {
+         description: 'Bad request',
+     }
+   */
+
+  };
+
+  exports.deleteItem = async (req, res) => {
+
+    /* 
+      #swagger.tags = ['Items']
+      #swagger.description = ''
+      #swagger.summary = ''
+      #swagger.parameters['id'] = {
+          description: '',
+          required: true,
+      }
+
+      #swagger.responses[200] = {
+          description: '',
+      }
+      #swagger.responses[400] = {
+          description: '',
+      }
+    */
+      
+  }
+
+  exports.getItem = async (req, res) => {
+
+    /* 
+      #swagger.tags = ['Items']
+      #swagger.description = 'Get an item entry'
+      #swagger.summary = 'Get an item entry'
+      #swagger.parameters['id'] = {
+          description: 'Item id',
+          required: true,
+      }
+      #swagger.responses[404] = {
+          description: 'Item not found',
+      }
+      #swagger.responses[400] = {
+          description: 'Bad request',
+      }
+      #swagger.responses[200] = {
+          description: 'Get an item by id',
+      }
+    */
+    
+  }
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const itemController = require('../controllers/itemController');
+const itemController = require('../controllers/itemControllers');
 
 router.post('/items', itemController.createItem);
 router.get('/items', itemController.getAllItems);
